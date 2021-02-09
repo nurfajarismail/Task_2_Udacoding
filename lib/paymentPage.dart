c90a75a222ca6f519eb05af922eda2d9078d460b
nurfajarismail <62985196+nurfajarismail@users.noreply.github.com>
2021-02-09 17:40:36 +0700

    change launcher icon

--- lib/paymentPage.dart
+++ lib/paymentPage.dart
@@ -15,124 +15,127 @@ class PaymentPage extends StatelessWidget {
           textAlign: TextAlign.center,
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(bottom: 10),
-              child: TextField(
-                decoration: InputDecoration(
-                    // enabledBorder: UnderlineInputBorder(
-                    //   borderSide: BorderSide(color: Colors.purple),
-                    // ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.purple),
-                    ),
-                    // border: UnderlineInputBorder(
-                    //     borderSide: BorderSide(color: Colors.purple)),
-                    labelStyle: TextStyle(color: Colors.grey),
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    icon: Icon(
-                      Icons.person,
-                      color: Colors.grey,
-                    ),
-                    labelText: "Card Name",
-                    hoverColor: Colors.purple,
-                    focusColor: Colors.purple),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(bottom: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      // enabledBorder: UnderlineInputBorder(
+                      //   borderSide: BorderSide(color: Colors.purple),
+                      // ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.purple),
+                      ),
+                      // border: UnderlineInputBorder(
+                      //     borderSide: BorderSide(color: Colors.purple)),
+                      labelStyle: TextStyle(color: Colors.grey),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      icon: Icon(
+                        Icons.person,
+                        color: Colors.grey,
+                      ),
+                      labelText: "Card Name",
+                      hoverColor: Colors.purple,
+                      focusColor: Colors.purple),
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.only(bottom: 10),
-              child: TextField(
-               // maxLength: 19,
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                  new CustomInputNumber()
-                ],
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    counterText: "",
-                    // enabledBorder: UnderlineInputBorder(
-                    //   borderSide: BorderSide(color: Colors.purple),
-                    // ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.purple),
-                    ),
-                    // border: UnderlineInputBorder(
-                    //     borderSide: BorderSide(color: Colors.purple)),
-                    labelStyle: TextStyle(color: Colors.grey),
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    icon: Icon(
-                      Icons.credit_card,
-                      color: Colors.grey,
-                    ),
-                    labelText: "Number"),
+              Container(
+                padding: EdgeInsets.only(bottom: 20),
+                child: TextField(
+                  // maxLength: 19,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    new CustomInputNumber()
+                  ],
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      counterText: "",
+                      // enabledBorder: UnderlineInputBorder(
+                      //   borderSide: BorderSide(color: Colors.purple),
+                      // ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.purple),
+                      ),
+                      // border: UnderlineInputBorder(
+                      //     borderSide: BorderSide(color: Colors.purple)),
+                      labelStyle: TextStyle(color: Colors.grey),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      icon: Icon(
+                        Icons.credit_card,
+                        color: Colors.grey,
+                      ),
+                      labelText: "Number"),
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.only(bottom: 10),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    // enabledBorder: UnderlineInputBorder(
-                    //   borderSide: BorderSide(color: Colors.purple),
-                    // ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.purple),
-                    ),
-                    // border: UnderlineInputBorder(
-                    //     borderSide: BorderSide(color: Colors.purple)),
-                    labelStyle: TextStyle(color: Colors.grey),
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    icon: Icon(
-                      Icons.card_membership,
-                      color: Colors.grey,
-                    ),
-                    labelText: "CVV"),
+              Container(
+                padding: EdgeInsets.only(bottom: 20),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      // enabledBorder: UnderlineInputBorder(
+                      //   borderSide: BorderSide(color: Colors.purple),
+                      // ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.purple),
+                      ),
+                      // border: UnderlineInputBorder(
+                      //     borderSide: BorderSide(color: Colors.purple)),
+                      labelStyle: TextStyle(color: Colors.grey),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      icon: Icon(
+                        Icons.card_membership,
+                        color: Colors.grey,
+                      ),
+                      labelText: "CVV"),
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.only(bottom: 10),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                  new CustomInputExpiry()
-                ],
-                maxLength: 5,
-                decoration: InputDecoration(
-                    counterText: "",
-                    // enabledBorder: UnderlineInputBorder(
-                    //   borderSide: BorderSide(color: Colors.purple),
-                    // ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.purple),
-                    ),
-                    // border: UnderlineInputBorder(
-                    //     borderSide: BorderSide(color: Colors.purple)),
-                    labelStyle: TextStyle(color: Colors.grey),
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    icon: Icon(
-                      Icons.date_range,
-                      color: Colors.grey,
-                    ),
-                    labelText: "Expiry Date"),
+              Container(
+                padding: EdgeInsets.only(bottom: 20),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    new CustomInputExpiry()
+                  ],
+                  maxLength: 5,
+                  decoration: InputDecoration(
+                      counterText: "",
+                      // enabledBorder: UnderlineInputBorder(
+                      //   borderSide: BorderSide(color: Colors.purple),
+                      // ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.purple),
+                      ),
+                      // border: UnderlineInputBorder(
+                      //     borderSide: BorderSide(color: Colors.purple)),
+                      labelStyle: TextStyle(color: Colors.grey),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      icon: Icon(
+                        Icons.date_range,
+                        color: Colors.grey,
+                      ),
+                      labelText: "Expiry Date"),
+                ),
               ),
-            ),
-            FlatButton(
-              onPressed: () {},
-              child: Text(
-                "Pay",
-                style: TextStyle(color: Colors.white),
-              ),
-              color: Colors.blue,
-            )
-          ],
+              FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "Pay",
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue,
+              )
+            ],
+          ),
         ),
       ),
     );
